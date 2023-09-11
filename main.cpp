0c7d2a7452283fd74f721ee8103e7f3cfeddd521
João Victor Rodrigues Santos <joao1203univali@gmail.com>
2023-09-11 13:17:10 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,12 +36,18 @@ void desenhar_bomba(int i, int j, int m[TAM][TAM]){
     }
 }
 
-void mapa(int x, int y, int m[TAM][TAM], int &bomba, int s, int d){
+void mapa(int x, int y, int m[TAM][TAM], int &bomba, int s, int d, int &inimigo, int &jogador){
+        if(m[s][d] == 4){
+            inimigo = 0;
+        }
+        if(m[x][y] == 4){
+        jogador = 0;
+        }
         for(int i=0;i<TAM;i++){
             for(int j=0;j<TAM;j++){
                 if(i==x && j==y){
                     cout<<char(2); //personagem
-                } else if(i == s && j == d){
+                } else if((i == s && j == d) && inimigo != 0) {
                     cout << char(190);
                 } else {
                             switch (m[i][j]){
@@ -49,14 +55,13 @@ void mapa(int x, int y, int m[TAM][TAM], int &bomba, int s, int d){
                                 case 1: cout<<char(219); break; //parede
                                 case 2: cout <<char(178);  break; // parede que pode ser quebrada
                                 case 3: if(bomba != 0) { break; } else { cout << char(208); bomba = bomba + 1; desenhar_bomba(i,j, m); } break; //bomba
-                                case 4: cout << char(158); m[i][j] = 0; bomba = 0; break; // explosão bomba
+                                case 4: cout << char(158); tempo(5); m[i][j] = 0;bomba = 0; break; // explosÃ£o bomba
                             }
                         }
             }
             cout<<"\n";
         }
 }
-
 
 int quantidade(int number, int m[TAM][TAM]){
     int contador = 0;
@@ -102,9 +107,9 @@ int verificar_posicao(int &x, int &y, int m[TAM][TAM], char tecla){
             }
 }
 
-    void mover(int &x,int &y, int m[TAM][TAM], int inimigo) {
-        if(inimigo == 1){
-                   int direcao = rand() % 4; // 0: cima, 1: baixo, 2: esquerda, 3: direita
+void mover(int &x,int &y, int m[TAM][TAM], int inimigo) {
+        if(inimigo != 0){
+            int direcao = rand() % 4; // 0: cima, 1: baixo, 2: esquerda, 3: direita
         switch (direcao) {
             case 0:
                 if(m[x-1][y] == 1 || m[x-1][y] == 2){
@@ -151,7 +156,7 @@ int main(){
         coord.X = CX;
 
         coord.Y = CY;
-//5 = INIMIGO
+
         int m[TAM][TAM] = {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
@@ -160,37 +165,37 @@ int main(){
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 1},
             {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2, 0, 0, 0, 1},
             {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 1},
-            {1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 1},
-            {1, 0, 0, 0, 2, 5, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1},
+            {1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1},
             {1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1},
-            {1, 0, 2, 0, 0, 0, 1, 0, 1, 0, 0, 0, 2, 0, 1},
+            {1, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 1},
             {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1},
             {1, 2, 0, 0, 0, 0, 2, 0, 0, 1, 1, 0, 2, 0, 1},
             {1, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         };
 
-        int x=5, y=5, bomba = 0, s = 8, d = 5, inimigo = 1, paredes = 0;
+        int x=5, y=5, bomba = 0, s = 8, d = 5, inimigo = 1, paredes = 0, jogador = 1;
         char tecla;
         bool jogo = true;
         while(jogo == true){
             paredes = quantidade(2, m);
             SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-            mapa(x,y, m, bomba, s,d);
+            mapa(x,y, m, bomba, s,d, inimigo, jogador);
             if (_kbhit()){
                 tecla = getch();
                 verificar_posicao(x,y, m, tecla);
                 mover(s, d, m, inimigo);
-            } else if(x == s && y == d){
+            } else if((x == s && y == d) || jogador == 0){
                 setlocale(LC_ALL, "Portuguese");
                 system("cls");
                 jogo = false;
-                cout << "Você Perdeu! O inimigo te capturou";
+                cout << "VocÃª Perdeu! O inimigo te capturou ou a bomba explodiu no jogador";
             } else if(paredes == 0 || inimigo == 0){
                 setlocale(LC_ALL, "Portuguese");
                 system("cls");
                 jogo = false;
-                cout << "Você Ganhou!";
+                cout << "VocÃª Ganhou!";
             }
         }
     return 0;
